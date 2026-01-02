70e2d0b0827b50ecd17698ea36c478bf0b973e1f
Vishesh-techno <sonivishesh099@gmail.com>
2026-01-02 11:58:37 +0530

    Program of Today

--- src/SecondJanuary.java
+++ src/SecondJanuary.java
@@ -28,8 +28,51 @@ public class SecondJanuary {
         return true;
     }
 
+    public static boolean isHappyII(int n) {
+        if (n == 1 || n == 7) {
+            return true;
+        } else if (n < 10) {
+            return false;
+        } else {
+            int sum = 0;
+            while (n != 0) {
+                int temp = n % 10;
+                sum = sum + (temp * temp);
+                n = n / 10;
+            }
+            return isHappyII(sum);
+        }
+    }
+
+    public static boolean isHappyIII(int n) {
+        int slow = n, fast = n;
+
+        while (true) {
+            slow = sumOfSquareOfDigits(slow);
+            fast = sumOfSquareOfDigits(sumOfSquareOfDigits(fast));
+
+            if (fast == 1) {
+                return true;
+            }
+
+            if (fast == slow) {
+                return false;
+            }
+        }
+    }
+
     public static void main(String[] args) {
         if (isHappy(19)) {
+            System.out.println("The Given Number is Happy");
+        } else {
+            System.out.println("The Given Number is Not Happy");
+        }
+        if (isHappyII(15)) {
+            System.out.println("The Given Number is Happy");
+        } else {
+            System.out.println("The Given Number is Not Happy");
+        }
+        if (isHappyIII(9)) {
             System.out.println("The Given Number is Happy");
         } else {
             System.out.println("The Given Number is Not Happy");
