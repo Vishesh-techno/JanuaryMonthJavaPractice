046f288aaea6eaf5ba2e1ec246197b473818db53
Vishesh-techno <sonivishesh099@gmail.com>
2026-01-03 16:43:43 +0530

    Program of Today

--- src/ThirdJanuary.java
+++ src/ThirdJanuary.java
@@ -1,5 +1,4 @@
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class ThirdJanuary {
     public static boolean isIsomorphic(String s, String t) {
@@ -24,7 +23,46 @@ public class ThirdJanuary {
         return true;
     }
 
+    public static int[] intersect(int[] nums1, int[] nums2) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        List<Integer> list = new ArrayList<>();
+
+        for (int num : nums1) {
+            if (map.containsKey(num)) {
+                map.put(num, map.get(num) + 1);
+            } else {
+                map.put(num, 1);
+            }
+        }
+
+        for (int num : nums2) {
+            if (map.containsKey(num)) {
+                if (map.get(num) == 1) {
+                    map.remove(num);
+                } else {
+                    map.put(num, map.get(num) - 1);
+                }
+                list.add(num);
+            }
+        }
+
+        int[] arr = new int[list.size()];
+        int k = 0;
+        for (int num : list) {
+            arr[k] = num;
+            k++;
+        }
+        return arr;
+    }
+
+
     public static void main(String[] args) {
         System.out.println(isIsomorphic("egg", "lee"));
+
+        int[] nums1 = {1, 2, 2, 1};
+        int[] nums2 = {2, 2, 1};
+
+        System.out.println(Arrays.toString(intersect(nums1, nums2)));
+
     }
 }
