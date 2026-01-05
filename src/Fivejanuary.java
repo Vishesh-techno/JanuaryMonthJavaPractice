212b190c31beea17e76dffcfe48a8d1d4f9f3bde
Vishesh-techno <sonivishesh099@gmail.com>
2026-01-05 12:54:41 +0530

    Program of Today

--- src/Fivejanuary.java
+++ src/Fivejanuary.java
@@ -4,7 +4,27 @@ import java.util.Map;
 import java.util.Set;
 
 public class Fivejanuary {
+    public static int fourSumCountII(int[] nums1, int[] nums2, int[] nums3, int[] nums4) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i : nums1) {
+            for (int j : nums2) {
+                int key = i + j;
+                map.put(key, map.getOrDefault(key, 0) + 1);
+            }
+        }
+        int count = 0;
+        for (int i : nums3) {
+            for (int j : nums4) {
+                int key = -1 * (i + j);
+                count = count + map.getOrDefault(key, 0);
+            }
+        }
+        return count;
+    }
+
     public static int numJewelsInStones(String jewels, String stones) {
+        long startTime = System.nanoTime();
         Set<Character> set = new HashSet<>();
         int count = 0;
 
@@ -17,10 +37,14 @@ public class Fivejanuary {
                 count++;
             }
         }
+        long endTime = System.nanoTime();
+        long duration = endTime - startTime;
+        System.out.println("Time: " + duration);
         return count;
     }
 
     public static int numJewelsInStonesII(String jewels, String stones) {
+        long startTime = System.nanoTime();
         char[] ch = jewels.toCharArray();
         char[] ch1 = stones.toCharArray();
         int count = 0;
@@ -31,13 +55,24 @@ public class Fivejanuary {
                 }
             }
         }
+        long endTime = System.nanoTime();
+        long duration = endTime - startTime;
+        System.out.println("time: " + duration);
         return count;
     }
 
     public static void main(String[] args) {
         String jewels = "aA";
         String stones = "aAAbbbb";
-
+        System.out.println(numJewelsInStones(jewels, stones));
         System.out.println(numJewelsInStonesII(jewels, stones));
+
+        int[] nums1 = {4, 5, 6, 7};
+        int[] nums2 = {6, 7, 5, 4};
+        int[] nums3 = {3, 8, 5, 4};
+        int[] nums4 = {0, 1, 2, 3, 4};
+
+        int res = fourSumCountII(nums1, nums2, nums3, nums4);
+        System.out.println(res);
     }
 }
