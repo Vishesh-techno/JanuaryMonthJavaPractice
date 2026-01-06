73bd20da1b63000e64eecaab50e383816aec7ae6
Vishesh-techno <sonivishesh099@gmail.com>
2026-01-06 15:19:49 +0530

    Program of Today

--- src/SixJanuary.java
+++ src/SixJanuary.java
@@ -1,5 +1,7 @@
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class SixJanuary {
     public static int lengthOfLongestSubString(String str) {
@@ -26,9 +28,31 @@ public class SixJanuary {
         return maxLen;
     }
 
+    public static int lengthOfLongestSubStringII(String str) {
+        Set<Character> set = new HashSet<>();
+
+        int left = 0, right = 0;
+        int maxLen = 0;
+
+        while (right < str.length()) {
+            char currChar = str.charAt(right);
+
+            while (set.contains(currChar)) {
+                set.remove(currChar);
+                left++;
+            }
+            set.add(currChar);
+            maxLen = Math.max(maxLen, right - left + 1);
+//            map.put(currChar, right);
+            right++;
+        }
+        return maxLen;
+    }
+
     public static void main(String[] args) {
         String str = "abcabcbb";
 
         System.out.println(lengthOfLongestSubString(str));
+        System.out.println(lengthOfLongestSubStringII(str));
     }
 }
