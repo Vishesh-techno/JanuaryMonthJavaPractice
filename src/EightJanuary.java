e4f236767d7a06104c293a2814ea8a41cb4d2b5f
Vishesh-techno <sonivishesh099@gmail.com>
2026-01-08 16:32:21 +0530

    Program of Today

--- src/EightJanuary.java
+++ src/EightJanuary.java
@@ -54,10 +54,54 @@ public class EightJanuary {
         }
     }
 
+    public static List<List<Integer>> combinationSum2(int[] nums, int target) {
+        Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<>();
+        generateSum(0, nums, new ArrayList<>(), res, target);
+        return res;
+    }
+
+    private static void generateSum(int idx, int[] nums, List<Integer> curr, List<List<Integer>> res, int target) {
+        if (target == 0) {
+            res.add(new ArrayList<>(curr));
+            return;
+        }
+        if (target < 0) return;
+        for (int i = idx; i < nums.length; i++) {
+            if (i > idx && nums[i] == nums[i - 1]) continue;
+            curr.add(nums[i]);
+            generateSum(i + 1, nums, curr, res, target - nums[i]);
+            curr.removeLast();
+        }
+    }
+
+    public static List<List<Integer>> combinationSum(int[] nums, int target) {
+        List<List<Integer>> res = new ArrayList<>();
+        generateSumSubset(0, nums, new ArrayList<>(), res, target);
+        return res;
+    }
+
+    private static void generateSumSubset(int idx, int[] nums, List<Integer> curr, List<List<Integer>> res, int target) {
+        if (target == 0) {
+            res.add(new ArrayList<>(curr));
+            return;
+        }
+        if (target < 0) return;
+        for (int i = idx; i < nums.length; i++) {
+//            if(i > idx && nums[i] == nums[i-1]) continue;
+            curr.add(nums[i]);
+            generateSum(i, nums, curr, res, target - nums[i]);
+            curr.removeLast();
+        }
+    }
 
     public static void main(String[] args) {
         int[] nums = {3, 3, 0, 3};
-//        System.out.println(permutations(nums));
+        System.out.println(permutations(nums));
         System.out.println(uniquePermutations(nums));
+        int[] num = {10, 1, 2, 7, 6, 1, 5};
+        System.out.println(combinationSum2(num, 8));
+        int[] arr = {2, 3, 6, 7};
+        System.out.println(combinationSum(arr, 7));
     }
 }
