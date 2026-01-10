35f29bede7f5b0cf687f33067b80e407597ec9fd
Vishesh-techno <sonivishesh099@gmail.com>
2026-01-10 11:27:49 +0530

    Program of Today

--- src/TenJanuary.java
+++ src/TenJanuary.java
@@ -1,4 +1,6 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class TenJanuary {
@@ -17,6 +19,61 @@ public class TenJanuary {
         return second;
     }
 
+    public static List<List<Integer>> subset(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        generateSubset(0, nums, new ArrayList<>(), res);
+        return res;
+    }
+
+    private static void generateSubset(int idx, int[] nums, List<Integer> curr, List<List<Integer>> res) {
+        res.add(new ArrayList<>(curr));
+        for (int i = idx; i < nums.length; i++) {
+            curr.add(nums[i]);
+            generateSubset(i + 1, nums, curr, res);
+            curr.removeLast();
+        }
+    }
+
+    public static List<List<Integer>> combination(int n, int k) {
+        List<List<Integer>> res = new ArrayList<>();
+        generateCombination(1, n, new ArrayList<>(), res, k);
+        return res;
+    }
+
+    private static void generateCombination(int idx, int n, List<Integer> curr, List<List<Integer>> res, int k) {
+        if (curr.size() == k) {
+            res.add(new ArrayList<>(curr));
+            return;
+        }
+        for (int i = idx; i <= n; i++) {
+            curr.add(i);
+            generateCombination(i + 1, n, curr, res, k);
+            curr.removeLast();
+        }
+    }
+
+    public static List<List<Integer>> permutation(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        boolean[] used = new boolean[nums.length];
+        generatePermutaion(nums, used, new ArrayList<>(), res);
+        return res;
+    }
+
+    private static void generatePermutaion(int[] nums, boolean[] used, List<Integer> curr, List<List<Integer>> res) {
+        if (curr.size() == nums.length) {
+            res.add(new ArrayList<>(curr));
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (used[i]) continue;
+            used[i] = true;
+            curr.add(nums[i]);
+            generatePermutaion(nums, used, curr, res);
+            used[i] = false;
+            curr.removeLast();
+        }
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n;
@@ -33,5 +90,9 @@ public class TenJanuary {
 
         String str = "Vishesh";
         System.out.println("\"" + str + "\"");
+
+        System.out.println("The Subsets are: " + subset(arr));
+        System.out.println("The Combinations are: " + combination(3, 2));
+        System.out.println("The Permutations are: " + permutation(arr));
     }
 }
