6780ec90e65041b4133b9ceeb7fc706508cde4fc
Vishesh-techno <sonivishesh099@gmail.com>
2026-01-15 19:46:31 +0530

    Program of Today

---
+++ src/FifteenJanuary.java
@@ -0,0 +1,29 @@
+public class FifteenJanuary {
+    public static boolean checkStraightLine(int[][] coordinates) {
+        if (coordinates.length <= 2) {
+            return false;
+        }
+
+        int deltaY21 = coordinates[1][1] - coordinates[0][1];
+        int deltaX21 = coordinates[1][0] - coordinates[0][0];
+        for (int i = 2; i < coordinates.length; i++) {
+            int deltaYI1 = coordinates[i][1] - coordinates[0][1];
+            int deltaXI1 = coordinates[i][0] - coordinates[0][0];
+
+            if (deltaY21 * deltaXI1 != deltaYI1 * deltaX21) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int[][] coordinates = {{1, 1}, {2, 2}, {3, 3}, {4, 4}, {5, 5}};
+
+        if (checkStraightLine(coordinates)) {
+            System.out.println("the coordinates in Straight Line");
+        } else {
+            System.out.println("the coordinates not in a Straight Line");
+        }
+    }
+}
