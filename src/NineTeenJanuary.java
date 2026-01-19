18b23b76f510f68def02ef73712b9e56364ac656
Vishesh-techno <sonivishesh099@gmail.com>
2026-01-19 20:30:31 +0530

    Program of Today

---
+++ src/NineTeenJanuary.java
@@ -0,0 +1,68 @@
+public class NineTeenJanuary {
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        public ListNode() {
+        }
+
+        public ListNode(int val) {
+            this.val = val;
+            this.next = null;
+        }
+    }
+
+    static ListNode head;
+
+    public static void add(int val) {
+        ListNode temp = head;
+        ListNode newNode = new ListNode(val);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        temp.next = newNode;
+    }
+
+    public static void printList() {
+        ListNode temp = head;
+        while (temp != null) {
+            System.out.print(temp.val + "==>");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public static ListNode swapPairs(ListNode head) {
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        ListNode p = dummy;
+        while (head != null && head.next != null) {
+            ListNode first = head;
+            ListNode second = head.next;
+
+            first.next = second.next;
+            second.next = first;
+            p.next = second;
+            p = first;
+            head = first.next;
+        }
+        return dummy.next;
+    }
+
+    public static void main(String[] args) {
+        add(6);
+        add(5);
+        add(4);
+        add(3);
+        add(2);
+        add(1);
+        printList();
+        head = swapPairs(head);
+        printList();
+    }
+}
+
