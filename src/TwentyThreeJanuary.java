aa78e30a9a54444c943b5e350662eb61d86d1ef1
Vishesh-techno <sonivishesh099@gmail.com>
2026-01-23 20:45:50 +0530

    Program of Today

--- src/TwentyThreeJanuary.java
+++ src/TwentyThreeJanuary.java
@@ -26,7 +26,7 @@ public class TwentyThreeJanuary {
         int maxSum = arr[0];
         int currSum = arr[0];
 
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 1; i < arr.length; i++) {
             if (currSum < 0) {
                 currSum = arr[i];
             } else {
@@ -39,8 +39,45 @@ public class TwentyThreeJanuary {
         return maxSum;
     }
 
+    public static void printArray(int[] nums) {
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+
+    public static void quickSort(int[] arr, int start, int end) {
+        if (start >= end) {
+            return;
+        }
+        int partIdx = partition(arr, start, end);
+        quickSort(arr, start, partIdx - 1);
+        quickSort(arr, partIdx + 1, end);
+    }
+
+    private static int partition(int[] arr, int start, int end) {
+        int pivot = arr[end];
+        int i = start - 1;
+
+        for (int j = start; j < end; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+                int temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
+            }
+        }
+//        i++;
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[end];
+        arr[end] = temp;
+        return i + 1;
+    }
+
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int[] arr = {6, 7, 10, 8, 9, 1, 2, 3, 4, 5};
         System.out.println(subArraySum(arr, 12));
+        quickSort(arr, 0, arr.length - 1);
+        printArray(arr);
     }
 }
