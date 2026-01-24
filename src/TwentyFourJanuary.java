0e21257c1a283f809d6f241c0a225a87342d1ba5
Vishesh-techno <sonivishesh099@gmail.com>
2026-01-24 13:13:05 +0530

    Program of Today

--- src/TwentyFourJanuary.java
+++ src/TwentyFourJanuary.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class TwentyFourJanuary {
     public static int maxSumSubArray(int[] nums) {
         int maxSum = Integer.MIN_VALUE;
@@ -14,8 +17,62 @@ public class TwentyFourJanuary {
         return maxSum;
     }
 
+    public static int minJumps(int[] nums) {
+        int n = nums.length;
+        int maxReach = 0, currReach = 0, jumps = 0;
+        for (int i = 0; i < n; i++) {
+            if (i > maxReach) {
+                return -1;
+            }
+            if (i > currReach) {
+                currReach = maxReach;
+                jumps++;
+            }
+            int next = i + nums[i];
+            if (maxReach < next) {
+                maxReach = next;
+            }
+        }
+        return jumps;
+    }
+
+    public static boolean isValidJump(int[] nums) {
+        int n = nums.length;
+        int currReach = 0, maxReach = 0;
+        for (int i = 0; i < n; i++) {
+            if (i > maxReach) return false;
+            if (i > currReach) currReach = maxReach;
+            int nextJump = i + nums[i];
+            if (maxReach < nextJump) {
+                maxReach = nextJump;
+            }
+        }
+        return true;
+    }
+
+    public static List<Integer> leaders(int[] nums) {
+        int n = nums.length;
+        List<Integer> res = new ArrayList<>();
+        for (int i = 0; i < n - 1; i++) {
+            int max = nums[i];
+            for (int j = i + 1; j < n; j++) {
+                if (max < nums[j]) {
+                    max = nums[j];
+                }
+            }
+            if (nums[i] == max) {
+                res.add(max);
+            }
+        }
+        res.add(nums[n - 1]);
+        return res;
+    }
+
     public static void main(String[] args) {
-        int[] nums = {1, 2, 5, 4, 5, 3, 145, 65, 52, 41, 5, -1100, 3, 2, 45, -3000, 3, 21, 5, 5, 4, 415, -200, 524, 100, 5, 5, 5};
+        int[] nums = {2, 3, 8, 1, 5, 5};
         System.out.println(maxSumSubArray(nums));
+        System.out.println(minJumps(nums));
+        System.out.println(isValidJump(nums));
+        System.out.println(leaders(nums));
     }
 }
