23baa777fa7d9cdebe7bb0f48f003785e90fe02c
Vishesh-techno <sonivishesh099@gmail.com>
2026-01-28 12:38:40 +0530

    Program of Today

--- src/TwentyEightJanuary.java
+++ src/TwentyEightJanuary.java
@@ -18,9 +18,22 @@ public class TwentyEightJanuary {
         }
     }
 
+    public static int countDigit(int num) { // count the number of digits in num that divide num
+        int n = num, count = 0;
+        while (n > 0) {
+            int lastDigit = n % 10;
+            n = n / 10;
+            if (lastDigit != 0 && num % lastDigit == 0) {
+                count++;
+            }
+        }
+        return count;
+    }
+
     public static void main(String[] args) {
         int[] nums = {2, 4, 6, 8, 10, 12, 14, 16, 18, 20};
         rotateArray(nums, 3);
         System.out.println(Arrays.toString(nums));
+        System.out.println(countDigit(4419));
     }
 }
