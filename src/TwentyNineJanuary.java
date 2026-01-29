48d300dd602f6f67a49eaabc8155b3160c737b9b
Vishesh-techno <sonivishesh099@gmail.com>
2026-01-29 11:29:42 +0530

    Program of Today

--- src/TwentyNineJanuary.java
+++ src/TwentyNineJanuary.java
@@ -1,4 +1,7 @@
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 
 public class TwentyNineJanuary {
     public static boolean isSubset(int[] a, int[] b) {
@@ -18,10 +21,22 @@ public class TwentyNineJanuary {
         return true;
     }
 
+    public static ArrayList<Integer> findUnion(int[] a, int[] b) {
+        Set<Integer> set = new HashSet<>();
+        for (int i : a) {
+            set.add(i);
+        }
+        for (int i : b) {
+            set.add(i);
+        }
+        return new ArrayList<>(set);
+    }
+
     public static void main(String[] args) {
         int[] a = {11, 7, 1, 13, 21, 3, 7, 3};
         int[] b = {11, 3, 7, 1, 7};
 
         System.out.println(isSubset(a, b));
+        System.out.println(findUnion(a, b));
     }
 }
