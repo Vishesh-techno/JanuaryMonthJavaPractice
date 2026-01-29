7b8ec225db5bae6f1d15330c0d15a7fbc9b6613a
Vishesh-techno <sonivishesh099@gmail.com>
2026-01-29 13:06:58 +0530

    Program of Today

--- src/TwentyNineJanuary.java
+++ src/TwentyNineJanuary.java
@@ -1,7 +1,4 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class TwentyNineJanuary {
     public static boolean isSubset(int[] a, int[] b) {
@@ -32,11 +29,88 @@ public class TwentyNineJanuary {
         return new ArrayList<>(set);
     }
 
+    public static ArrayList<Integer> findUnionII(int[] a, int[] b) {
+        Arrays.sort(a);
+        Arrays.sort(b);
+        ArrayList<Integer> res = new ArrayList<>();
+        int i = 0, j = 0;
+        while (i < a.length && j < b.length) {
+            if (a[i] == b[j]) {
+                if (res.size() == 0 || res.get(res.size() - 1) != a[i]) {
+                    res.add(a[i]);
+                }
+                i++;
+                j++;
+            } else if (a[i] < b[j]) {
+                if (res.size() == 0 || res.get(res.size() - 1) != a[i]) {
+                    res.add(a[i]);
+                }
+                i++;
+            } else {
+                if (res.size() == 0 || res.get(res.size() - 1) != b[j]) {
+                    res.add(b[j]);
+                }
+                j++;
+            }
+        }
+        while (i < a.length) {
+            if (res.size() == 0 || res.get(res.size() - 1) != a[i]) {
+                res.add(a[i]);
+            }
+            i++;
+        }
+
+        while (j < b.length) {
+            if (res.size() == 0 || res.get(res.size() - 1) != b[j]) {
+                res.add(b[j]);
+            }
+            j++;
+        }
+        return res;
+    }
+
+    public static ArrayList<Integer> nextLargerElement(int[] arr){
+        int n = arr.length;
+        ArrayList<Integer> res = new ArrayList<>();
+        for(int i=0; i<n; i++){
+            int nextGreater = -1;
+            for(int j=i+1; j<n; j++){
+                if(nextGreater < arr[j]){
+                    nextGreater = arr[j];
+                    break;
+                }
+            }
+            res.add(nextGreater);
+        }
+        return res;
+    }
+
+    public static ArrayList<Integer> nextLargerElementII(int[] arr){
+        ArrayList<Integer> res = new ArrayList<>();
+        Stack<Integer> stk = new Stack<>();
+        for(int i: arr){
+            res.add(-1);
+        }
+        for(int i=arr.length-1; i>=0; i--){
+            while (!stk.isEmpty() && stk.peek() <= arr[i]){
+                stk.pop();
+            }
+
+            if(!stk.isEmpty()){
+                res.set(i, stk.peek());
+            }
+
+            stk.push(arr[i]);
+        }
+        return res;
+    }
     public static void main(String[] args) {
         int[] a = {11, 7, 1, 13, 21, 3, 7, 3};
         int[] b = {11, 3, 7, 1, 7};
 
         System.out.println(isSubset(a, b));
         System.out.println(findUnion(a, b));
+        System.out.println(findUnionII(a, b));
+        System.out.println(nextLargerElementII(a));
     }
 }
