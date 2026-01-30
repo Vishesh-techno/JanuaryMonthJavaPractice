c2c87a6958d471e814985bca6dbef1753f119064
Vishesh-techno <sonivishesh099@gmail.com>
2026-01-30 12:09:07 +0530

    Program of Today

--- src/ThirtyJanuary.java
+++ src/ThirtyJanuary.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class ThirtyJanuary {
     public static int maxProductSubArray(int[] nums) {
         int prod = 1;
@@ -16,8 +18,24 @@ public class ThirtyJanuary {
         return max;
     }
 
+    public static int[] maxOfSubArrays(int[] nums, int k) {
+        int n = nums.length;
+        int[] res = new int[n - k + 1];
+        for (int i = 0; i <= n - k; i++) {
+            int max = nums[i];
+            for (int j = i; j < i + k; j++) {
+                if (nums[j] > max) {
+                    max = nums[j];
+                }
+            }
+            res[i] = max;
+        }
+        return res;
+    }
+
     public static void main(String[] args) {
         int[] nums = {-2, 6, -3, -10, 0, 2};
         System.out.println(maxProductSubArray(nums));
+        System.out.println(Arrays.toString(maxOfSubArrays(nums, 3)));
     }
 }
